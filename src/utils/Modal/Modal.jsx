c3e9252b82c47569418cf85c8f1fa4e73ca9c0b3
PavloZiuskin <passzuskin@gmail.com>
2023-06-25 01:01:03 +0300

    add-modal-notstyled

---
+++ src/utils/Modal/Modal.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+const rootModal = document.querySelector('#modal');
+
+const Modal = ({ children, onClose }) => {
+  useEffect(() => {
+    const handleEscape = e => {
+      if (e.code === `Escape`) {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleEscape);
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+    };
+  }, [onClose]);
+
+  const handleClose = e => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return createPortal(
+    <div onMouseDown={handleClose}>
+      <div>
+        <button type="button" onClick={onClose}></button>
+        {children}
+      </div>
+    </div>,
+    rootModal
+  );
+};
