ec2ae9384db8f9d347ca4f0b7177a38192d1dee4
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-06-24 12:21:56 +0300

    add style and validation

--- src/pages/LoginForm/LoginForm.js
+++ src/pages/LoginForm/LoginForm.js
@@ -1,30 +1,38 @@
 import css from './LoginForm.module.css';
-
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import { useState } from 'react';
+import * as Yup from 'yup';
+import { MdOutlineLogin } from 'react-icons/md';
+import { BiErrorCircle } from 'react-icons/bi';
+import { FcOk } from 'react-icons/fc';
 
 export const LoginForm = () => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const [loginForm, setLoginForm] = useState({});
 
   const handleSumbit = (values, { resetForm }) => {
-    console.log(values);
-    if (values.password === '') {
-      alert('password or email is going wrong');
-      return;
-    }
     setLoginForm(values);
     resetForm();
   };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('This is an ERROR email')
+      .matches(emailRegex, 'This is an ERROR email')
+      .required('Email is required'),
+    password: Yup.string().min(3).required('Password is required'),
+  });
 
   console.log(loginForm);
 
   return (
     <div className={css.main_container}>
       <div className={css.inside_container}>
-        <h3 className={css.title}> Log In</h3>
+        <h3 className={css.title}>Log In</h3>
         <Formik
           initialValues={{ email: '', password: '' }}
           onSubmit={handleSumbit}
+          validationSchema={validationSchema}
         >
           {({
             values,
@@ -32,17 +40,23 @@ export const LoginForm = () => {
             touched,
             handleChange,
             handleBlur,
-            handleSubmit,
             isSubmitting,
             /* and other goodies */
           }) => (
-            <Form onSubmit={handleSubmit} className={css.form}>
+            <Form className={css.form}>
               <div className={css.email_box}>
-                <label htmlFor="email" className={css.label_email}>
+                <label
+                  htmlFor="email"
+                  className={`${css.label_email} ${
+                    touched.email && errors.email ? css.error_label : ''
+                  } ${touched.email && !errors.email ? css.success_label : ''}`}
+                >
                   Email
                 </label>
                 <Field
-                  className={css.field}
+                  className={`${css.field} ${
+                    touched.email && errors.email ? css.error : ''
+                  } ${touched.email && !errors.email ? css.success : ''}`}
                   type="email"
                   name="email"
                   onChange={handleChange}
@@ -50,14 +64,45 @@ export const LoginForm = () => {
                   value={values.email}
                   placeholder={'nadiia@gmail.com'}
                 />
-                {errors.email && touched.email && errors.email}
+                <ErrorMessage
+                  name="email"
+                  component="p"
+                  className={css.error_message}
+                />
+                {touched.email && errors.email ? (
+                  <BiErrorCircle className={css.iconError} />
+                ) : (
+                  ''
+                )}
+
+                {!errors.email && values.email !== '' ? (
+                  <>
+                    <p className={css.success_message}>
+                      This is an CORRECT email
+                    </p>
+                    <FcOk className={css.iconOk} />
+                  </>
+                ) : (
+                  ''
+                )}
               </div>
               <div className={css.password_box}>
-                <label htmlFor="password" className={css.label_email}>
+                <label
+                  htmlFor="password"
+                  className={`${css.label_email} ${
+                    touched.password && errors.password ? css.error_label : ''
+                  } ${
+                    touched.password && !errors.password
+                      ? css.success_label
+                      : ''
+                  }`}
+                >
                   Password
                 </label>
                 <Field
-                  className={css.field}
+                  className={`${css.field} ${
+                    touched.password && errors.password ? css.error : ''
+                  } ${touched.password && !errors.password ? css.success : ''}`}
                   type="password"
                   name="password"
                   onChange={handleChange}
@@ -65,19 +110,26 @@ export const LoginForm = () => {
                   value={values.password}
                   placeholder={'password'}
                 />
-                {errors.password && touched.password && errors.password}
+                <ErrorMessage
+                  name="password"
+                  component="p"
+                  className={css.error_message}
+                />
               </div>
               <button
                 type="submit"
                 // disabled={isSubmitting}
                 className={css.button}
               >
-                Log in
+                Log in{<MdOutlineLogin className={css.logIn} />}
               </button>
             </Form>
           )}
         </Formik>
       </div>
+      <a href="/" className={css.sign}>
+        Sign up
+      </a>
     </div>
   );
 };
