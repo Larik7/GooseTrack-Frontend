cf1683db51b0223abe43ea6989bde835f131c2c6
Transcendence <baydindenys@ukr.net>
2023-06-25 20:38:38 +0300

    files

---
+++ src/components/MainPage/Description/Description.styled.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { useResponse } from 'hooks';
+import {
+  Main,
+  Wrapper,
+  Section,
+  InfoBox,
+  InfoBoxReverse,
+  ImageWrapper,
+  Number,
+  ColorTitle,
+  SubTitle,
+  Text,
+} from './Description.styled';
+
