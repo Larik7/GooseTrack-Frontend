a20b5287ace09d4c371db07ce8159ec961f07749
Transcendence <baydindenys@ukr.net>
2023-06-25 20:42:55 +0300

    import photos
    import photos

--- src/components/MainPage/Description/Description.jsx
+++ src/components/MainPage/Description/Description.jsx
@@ -14,3 +14,26 @@ import {
   Text,
 } from './Description.styled';
 
+//desktop_images
+import d_calendar from 'images/mainPage/desktop/desktop_calendar.png';
+import d_calendar2 from 'images/mainPage/desktop/desktop_calendar@2x.png';
+import d_sidebar from 'images/mainPage/desktop/desktop_sidebar.png';
+import d_sidebar2 from 'images/mainPage/desktop/desktop_sidebar@2x.png';
+import d_all from 'images/mainPage/desktop/desktop_all.png';
+import d_all2 from 'images/mainPage/desktop/desktop_all@2x.png';
+
+//tablet_images
+import t_calendar from 'images/mainPage/tablet/tablet_calendar.png';
+import t_calendar2 from 'images/mainPage/tablet/tablet_calendar@2x.png';
+import t_sidebar from 'images/mainPage/tablet/tablet_sidebar.png';
+import t_sidebar2 from 'images/mainPage/tablet/tablet_sidebar@2x.png';
+import t_all from 'images/mainPage/tablet/tablet_all.png';
+import t_all2 from 'images/mainPage/tablet/tablet_all@2x.png';
+
+//mobile_images
+import m_calendar from 'images/mainPage/mobile/mobile_calendar.png';
+import m_calendar2 from 'images/mainPage/mobile/mobile_calendar@2x.png';
+import m_sidebar from 'images/mainPage/mobile/mobile_sidebar.png';
+import m_sidebar2 from 'images/mainPage/mobile/mobile_sidebar@2x.png';
+import m_all from 'images/mainPage/mobile/mobile_all.png';
+import m_all2 from 'images/mainPage/mobile/mobile_all@2x.png';
