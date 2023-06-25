d8830aa2347c35ab4fbd8c3dedede9122e768c81
Anton Zvertanovsky <anton.zv2002@gmail.com>
2023-06-25 21:21:34 +0300

    bugfix/ useState  useEffect is defined but never used

--- src/pages/MainLayout/MainLayout.jsx
+++ src/pages/MainLayout/MainLayout.jsx
@@ -2,7 +2,7 @@
 import { Header } from "components/Header/Header"
 import { SideBar } from "components/SideBar/SideBar"
 import css from './mainlayout.module.css'
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
 
 export const MainLayout =()=>{
