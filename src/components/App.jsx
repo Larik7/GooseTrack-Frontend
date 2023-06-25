bc448b38706c5d6b2313e6a0ea9726712dea52a5
Anton Zvertanovsky <anton.zv2002@gmail.com>
2023-06-25 21:03:31 +0300

    добавив правильне використання

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import { LoginForm } from '../pages/LoginForm/LoginForm';
 import { AuthSection } from '../components/AuthSection/AuthSection';
+import { MainLayout } from '../pages/MainLayout/MainLayout';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<AuthSection />}></Route>
       <Route path="/login" element={<LoginForm />}></Route>
-      <Route path="/authSection" element={<button>open AuthSection</button>}></Route>
+      <Route path="/mainLayout" element={<MainLayout />}></Route>
     </Routes>
   );
 };
