0be5ee3f65b1c1d76d2c1e5a5da17fbe4384fb03
Transcendence <baydindenys@ukr.net>
2023-06-27 23:12:03 +0300

    Revert "upd"
    This reverts commit 6cb279a8b1bc313bc3d146092d8894726ad27370.

--- src/components/MainPage/ReviewSlider/ReviewSlider.jsx
+++ src/components/MainPage/ReviewSlider/ReviewSlider.jsx
@@ -2,11 +2,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Slider from 'react-slick';
-<<<<<<< Updated upstream
 // import { fetchAllReviews } from 'redux/reviews/operations';
 // import { selectAllReviews } from 'redux/reviews/selectors';
-=======
->>>>>>> Stashed changes
 import { ReviewCard } from './ReviewCard';
 import {
   ReviewsContainer,
@@ -15,11 +12,7 @@ import {
 } from './ReviewsSlider.styled';
 import sprite from 'icons/sprite.svg';
 
-<<<<<<< Updated upstream
 // Slide Right
-=======
-
->>>>>>> Stashed changes
 const SlickRight = ({ currentSlide, slideCount, ...props }) => (
   <div
     {...props}
@@ -34,10 +27,7 @@ const SlickRight = ({ currentSlide, slideCount, ...props }) => (
   </div>
 );
 
-<<<<<<< Updated upstream
 // Slide Left
-=======
->>>>>>> Stashed changes
 const SlickLeft = ({ currentSlide, slideCount, ...props }) => (
   <div
     {...props}
@@ -73,10 +63,7 @@ const settings = {
   prevArrow: <SlickLeft />,
 };
 
-<<<<<<< Updated upstream
 // Review Slider
-=======
->>>>>>> Stashed changes
 export const ReviewsSlider = () => {
   const reviews = useSelector(selectAllReviews);
   const dispatch = useDispatch();
