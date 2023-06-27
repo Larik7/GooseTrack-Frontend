09b37c4d047f04421304ad9913833f0a89819cc4
Larik7 <Larik777taganka@gmail.com>
2023-06-27 19:21:43 +0300

    fixed

--- src/pages/MainLayout/MainLayout.jsx
+++ src/pages/MainLayout/MainLayout.jsx
@@ -2,42 +2,26 @@ import { Header } from 'components/Header/Header';
 import { SideBar } from 'components/SideBar/SideBar';
 import css from './mainlayout.module.css';
 import { CreateTaskModal } from 'components/CreateTaskModal/CreateTaskModal';
-import { useState } from 'react';
-
-
-import { Header } from "components/Header/Header"
-import { SideBar } from "components/SideBar/SideBar"
-import css from './mainlayout.module.css'
-
-
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 //   const [open, setOpen] = useState(false);
 
-export const MainLayout =()=>{
-const [openMenu, setOpen] = useState(true); 
-   
- 
+export const MainLayout = () => {
+  // const isViewportWideEnough = window.innerWidth >= 1440;
+  const [openMenu, setOpenMenu] = useState(true);
+  const [open, setOpen] = useState(false);
 
-   
-    return(
-<div className={css.conteinerMainLayout}>
-<Header openMenu={openMenu} setOpen={setOpen}  />
-<main className={css.main}>
- <SideBar openMenu={openMenu}/>
-
-    <div className={css.component} > Some component</div>
-</main>
-</div>
-
+  const onClose = () => {
+    setOpen(!open);
+  };
 
   return (
     <div className={css.conteinerMainLayout}>
-      <Header />
+      <Header openMenu={openMenu} setOpen={setOpenMenu} />
       <main className={css.main}>
         <SideBar />
         <div className={css.component}> Some component</div>
-
+        <button onClick={onClose}>open</button>
         {open && <CreateTaskModal onClose={onClose} />}
       </main>
     </div>
