1172fea1743ba19bd40d5249c1db7e1b96b28a60
Andrii Marchuk <andrii.marchuck@gmail.com>
2023-06-28 19:23:45 +0300

    add route UserInfo, next task - styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,18 @@ import { LoginPage } from '../pages/LoginPage/LoginPage';
 import { AuthSection } from '../components/AuthSection/AuthSection';
 import { MainLayout } from '../pages/MainLayout/MainLayout';
 import { RegisterPage } from 'pages/Register/RegisterPage';
+import { UserInfo } from './UserInfo/UserInfo';
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<AuthSection />}></Route>
       <Route path="/login" element={<LoginPage />}></Route>
       <Route path="/register" element={<RegisterPage />}></Route>
-      <Route path="/mainLayout" element={<MainLayout />}></Route>
+      {
+        <Route path="/mainLayout" element={<MainLayout />}>
+          <Route path="userInfo" element={<UserInfo />}></Route>
+        </Route>
+      }
     </Routes>
   );
 };
--- src/components/SideBar/UserNav/UserNav.jsx
+++ src/components/SideBar/UserNav/UserNav.jsx
@@ -6,7 +6,6 @@ import { BsBarChart } from 'react-icons/bs';
 // import { Routes, Route } from 'react-router-dom';
 // import { NavLink } from 'react-router-dom';
 
-
 // Зробив заглушку в посиланнях
 
 import css from './userNav.module.css';
@@ -14,17 +13,14 @@ import css from './userNav.module.css';
 export const UserNav = () => {
   return (
     <div className={css.linksConteiner}>
-      <a href="##" to="/account" className={css.link}>
-    
+      <a href="mainLayout/userInfo" to="/userInfo" className={css.link}>
         <RiUserFollowLine size={24} /> My account
       </a>
       <a href="##" to="/calendar" className={css.link}>
-      
         <LuCalendarCheck2 size={24} />
         Calendar{' '}
       </a>
       <a href="##" to="/statistics" className={css.link}>
-   
         <BsBarChart size={24} /> Statistics
       </a>
     </div>
