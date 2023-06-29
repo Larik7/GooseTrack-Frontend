5161fa2b3b1750c06597965eeb33e7eb7bf0e229
Andrii Marchuk <andrii.marchuck@gmail.com>
2023-06-29 14:53:08 +0300

    initial commit for UserInfo, add style for form

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,9 +1,35 @@
-import { Formik } from "formik";
+// import { Formik } from 'formik';
+import css from './UserInfo.module.css'
 
 export const UserInfo = () => {
   return (
-    <Formik>
-      
-    </Formik>
+    <form className={css.userInfoForm}>
+      <label>
+        <input type="file" />
+      </label>
+      <h2>Name</h2>
+      <p>Second name</p>
+
+      <label htmlFor="">
+        User name
+        <input type="text" />
+      </label>
+      <label htmlFor="">
+        Birthday
+        <input type="text" />
+      </label>
+      <label htmlFor="">
+        Email
+        <input type="text" />
+      </label>
+      <label htmlFor="">
+        Phone
+        <input type="text" />
+      </label>
+      <label htmlFor="">
+        Skype
+        <input type="text" />
+      </label>
+    </form>
   );
 };
