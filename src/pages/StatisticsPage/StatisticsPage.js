d3dbf012f360c0c21e4a50cd9892c1cb5b924891
AS <samaloanastasia@gmail.com>
2023-07-03 00:22:53 +0300

    fix page

--- src/pages/StatisticsPage/StatisticsPage.js
+++ src/pages/StatisticsPage/StatisticsPage.js
@@ -1,38 +1,14 @@
-import { Header } from 'components/Header/Header';
-import { SideBar } from 'components/SideBar/SideBar';
 import css from './StatisticsPage.module.css';
 
 import { StatisticsChart } from 'components/StatisticsChart/StatisticsChart';
 
-import React, { Suspense, useState } from 'react';
+import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 export const StatisticsPage = () => {
-  const [openMenu, setOpenMenu] = useState(true);
 
-  const hendelBackDropClick = e => {
-    if (e.target === e.currentTarget && !openMenu) {
-      console.log('кликнули');
-      setOpenMenu(!openMenu);
-    }
-  };
-
-  const onCloseSideBar = () => {
-    setOpenMenu(!openMenu);
-  };
 
   return (
-    <div className={css.conteinerMainLayout}>
-      <div className={!openMenu && css.backDrop} onClick={hendelBackDropClick}>
-        {' '}
-      </div>
-      <Header openMenu={openMenu} setOpen={setOpenMenu} />
-
-      <aside className={css.aside}>
-        <SideBar openMenu={openMenu} onClose={onCloseSideBar} />
-      </aside>
-
-      <main className={css.main}>
         <div className={css.mainComponent}>
           <Suspense fallback={null}>
             {
@@ -51,7 +27,5 @@ export const StatisticsPage = () => {
             <Outlet />
           </Suspense>
         </div>
-      </main>
-    </div>
   );
 };
