ad3c7a95c716c80e84941a5f9fda2893efb0e35a
Maksym Stankov <maksym.stankov@gmail.com>
2023-07-04 15:37:48 +0300

    TaskToolbar
    added:
    *draft toolbar
    *delete logic
    *some edit logic(not done)
    *tusk operation update tusk
    *tusk operation delete tusk

--- src/redux/tasks/taskOperation.js
+++ src/redux/tasks/taskOperation.js
@@ -44,3 +44,38 @@ export const addTask = createAsyncThunk(
     }
   }
 );
+
+export const updateTask = createAsyncThunk(
+  'task/update',
+  async ({ id, task }, { rejectWithValue, getState }) => {
+    const { auth } = getState();
+    const accessToken = auth.accessToken;
+    if (accessToken === null) {
+      return rejectWithValue('We dont have a token');
+    }
+    try {
+      const response = await axios.patch(`api/tasks/${id}`, task);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteTask = createAsyncThunk(
+  'task/delete',
+  async (taskId, { rejectWithValue, getState }) => {
+    console.log(taskId);
+    const { auth } = getState();
+    const accessToken = auth.accessToken;
+    if (accessToken === null) {
+      return rejectWithValue('We dont have a token');
+    }
+    try {
+      const response = await axios.delete(`api/tasks/${taskId}`);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
