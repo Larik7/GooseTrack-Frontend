f1faea59902064ea40227592e9b6aa98fad56b6f
Larisa Melnichenko <erlara@ukr.net>
2023-07-05 13:49:27 +0300

    fix-statistics

--- src/redux/tasks/selectors.js
+++ src/redux/tasks/selectors.js
@@ -1,4 +1,4 @@
-export const getAllTasks = state => state.task.tasks.allTasks;
+// export const getAllTasks = state => state.task.tasks.allTasks;
 export const getIsLoading = state => state.task.tasks.isLoading;
 export const getError = state => state.task.tasks.error;
 
