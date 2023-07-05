93a7576aa7ec4da0f6d41e3617488fe4d1113643
Transcendence <baydindenys@ukr.net>
2023-07-05 15:34:43 +0300

    styles fix
    description bg
    styles fix

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { AuthSection } from 'components/MainPage/AuthSection/AuthSection';
 import { Description } from 'components/MainPage/Description/Description';
-import { ReviewsSlider } from 'components/MainPage/ReviewSlider/ReviewSlider';
+import { ReviewsSlider } from 'components/MainPage/ReviewSlider/ReviewsSlider';
 // import { ScrollToTop } from 'components/ScrolButton/scrollButton';
 
 export const MainPage = () => {
