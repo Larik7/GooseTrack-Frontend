79d038277b4730cb6ec9489cc01d1a334307d608
Larik7 <larik777taganka@gmail.com>
2023-07-06 16:52:32 +0300

    test

--- src/components/Calendar/CalendarToolbar/PeriodPaginator/PeriodPaginator.jsx
+++ src/components/Calendar/CalendarToolbar/PeriodPaginator/PeriodPaginator.jsx
@@ -1,13 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { AiOutlineLeft } from 'react-icons/ai';
 import { format, isValid } from 'date-fns';
 import css from './PeriodPaginator.module.css';
 import { useNavigate, useParams } from 'react-router-dom';
 import { parseDate } from 'helpers/parseDate';
-import { enGB, eo, uk } from 'date-fns/locale';
-import { useTranslation } from 'react-i18next';
-
-const locales = { enGB, eo, uk };
 
 export const PeriodPaginator = ({
   selectedDay,
@@ -20,9 +16,6 @@ export const PeriodPaginator = ({
   const params = useParams();
   const navigate = useNavigate();
 
-  const { i18n } = useTranslation();
-  const [language, setLanguage] = useState(i18n.language);
-
   useEffect(() => {
     if (!params.currentDay) {
       return;
@@ -31,27 +24,15 @@ export const PeriodPaginator = ({
     navigate(`/calendar/day/${parsedDate}`);
   }, [selectedDay, navigate, params.currentDay]);
 
-  useEffect(() => {
-    if (i18n.language === 'ua') {
-      setLanguage('uk');
-    } else {
-      setLanguage('enGB');
-    }
-  }, [i18n.language]);
-
   const monthFormat = 'LLLL y';
   const dayFormat = 'd MMM y';
 
-  const formattedMonth = format(currentDate, monthFormat, {
-    locale: locales[language],
-  });
+  const formattedMonth = format(currentDate, monthFormat);
 
   let formattedDay = '';
   if (isValid(selectedDay)) {
     console.log('selectedDay', selectedDay);
-    formattedDay = format(selectedDay, dayFormat, {
-      locale: locales[language],
-    });
+    formattedDay = format(selectedDay, dayFormat);
   }
 
   const handlePrevClick = () => {
--- src/components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.jsx
+++ src/components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.jsx
@@ -1,9 +1,53 @@
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import css from 'components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.module.css';
+// import { parseDate } from 'helpers/parseDate';
+// import { useParams } from 'react-router-dom';
+// import { useTranslation } from 'react-i18next';
+
+// export const PeriodTypeSelect = ({
+//   setSelectedDay,
+//   selectedDay,
+//   currentDate,
+//   setCurrentDate,
+// }) => {
+//   const { i18n } = useTranslation();
+//   const params = useParams();
+//   const parsedDate = parseDate(currentDate);
+//   return (
+//     <div className={css.tabs_flex}>
+//       <div className={css.types_tabs_container}>
+//         <NavLink
+//           className={css.types_tabs}
+//           to="/calendar"
+//           end
+//           onClick={() => {
+//             setCurrentDate(selectedDay);
+//           }}
+//         >
+//           {i18n.language === 'en' ? 'Month' : 'Місяць'}
+//         </NavLink>
+//         <NavLink
+//           className={`${css.types_tabs} ${
+//             window.location.pathname.includes('/day/') ? 'active' : ''
+//           }`}
+//           to={`day/${parsedDate}`}
+//           onClick={() => {
+//             setSelectedDay(currentDate);
+//           }}
+//           disabled={params.currentDay ? true : false}
+//         >
+//           {i18n.language === 'en' ? 'Day' : 'День'}
+//         </NavLink>
+//       </div>
+//     </div>
+//   );
+// };
+
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation, useParams } from 'react-router-dom';
 import css from 'components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.module.css';
 import { parseDate } from 'helpers/parseDate';
-import { useParams } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
 
 export const PeriodTypeSelect = ({
   setSelectedDay,
@@ -11,33 +55,39 @@ export const PeriodTypeSelect = ({
   currentDate,
   setCurrentDate,
 }) => {
-  const { i18n } = useTranslation();
   const params = useParams();
+  const location = useLocation();
   const parsedDate = parseDate(currentDate);
+
+  const isDayActive = location.pathname.includes('/day/');
+  const isMonthActive = !isDayActive;
+
+  const handleMonthClick = () => {
+    setCurrentDate(selectedDay);
+  };
+
+  const handleDayClick = () => {
+    setSelectedDay(currentDate);
+  };
+
   return (
     <div className={css.tabs_flex}>
       <div className={css.types_tabs_container}>
         <NavLink
-          className={css.types_tabs}
+          className={`${css.types_tabs} ${isMonthActive ? css.active : ''}`}
           to="/calendar"
           end
-          onClick={() => {
-            setCurrentDate(selectedDay);
-          }}
+          onClick={handleMonthClick}
         >
-          {i18n.language === 'en' ? 'Month' : 'Місяць'}
+          Month
         </NavLink>
         <NavLink
-          className={`${css.types_tabs} ${
-            window.location.pathname.includes('/day/') ? 'active' : ''
-          }`}
+          className={`${css.types_tabs} ${isDayActive ? css.active : ''}`}
           to={`day/${parsedDate}`}
-          onClick={() => {
-            setSelectedDay(currentDate);
-          }}
-          disabled={params.currentDay ? true : false}
+          onClick={handleDayClick}
+          disabled={!!params.currentDay}
         >
-          {i18n.language === 'en' ? 'Day' : 'День'}
+          Day
         </NavLink>
       </div>
     </div>
