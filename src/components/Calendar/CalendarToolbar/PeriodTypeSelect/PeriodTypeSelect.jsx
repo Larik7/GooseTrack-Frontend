8d177f5458e768a03bc8b292a837da179619a475
Larik7 <larik777taganka@gmail.com>
2023-07-07 16:24:29 +0300

    fixed calendar

--- src/components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.jsx
+++ src/components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.jsx
@@ -1,9 +1,52 @@
+import React from 'react';
+import css from 'components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.module.css';
+import { parseDate } from 'helpers/parseDate';
+import { useParams, NavLink } from 'react-router-dom';
+
+export const PeriodTypeSelect = ({
+  setSelectedDay,
+  selectedDay,
+  currentDate,
+  setCurrentDate,
+}) => {
+  const params = useParams();
+  const parsedDate = parseDate(currentDate);
+
+  return (
+    <div className={css.tabs_flex}>
+      <div className={css.types_tabs_container}>
+        <NavLink
+          className={css.types_tabs}
+          to="/calendar"
+          exact
+          onClick={() => {
+            setCurrentDate(selectedDay);
+          }}
+        >
+          Month
+        </NavLink>
+        <NavLink
+          className={`${css.types_tabs} ${
+            window.location.pathname.includes('/day/') ? 'active' : ''
+          }`}
+          to={`day/${parsedDate}`}
+          onClick={() => {
+            setSelectedDay(currentDate);
+          }}
+          disabled={params.currentDay ? true : false}
+        >
+          Day
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+
 // import React from 'react';
-// import { NavLink } from 'react-router-dom';
 // import css from 'components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.module.css';
 // import { parseDate } from 'helpers/parseDate';
-// import { useParams } from 'react-router-dom';
-// import { useTranslation } from 'react-i18next';
+// import { useParams, NavLink } from 'react-router-dom';
 
 // export const PeriodTypeSelect = ({
 //   setSelectedDay,
@@ -11,7 +54,50 @@
 //   currentDate,
 //   setCurrentDate,
 // }) => {
-//   const { i18n } = useTranslation();
+//   const params = useParams();
+//   const parsedDate = parseDate(currentDate);
+//   return (
+//     <div className={css.tabs_flex}>
+//       <div className={css.types_tabs_container}>
+//       <NavLink
+//            className={css.types_tabs}
+//            to="/calendar"
+//            end
+//           onClick={() => {
+//              setCurrentDate(selectedDay);
+//            }}
+//          >
+//           Month
+//           </NavLink>
+//           <NavLink
+//          className={`${css.types_tabs} ${
+//             window.location.pathname.includes('/day/') ? 'active' : ''
+//           }`}
+//            to={`day/${parsedDate}`}
+//            onClick={() => {
+//              setSelectedDay(currentDate);
+//           }}
+//            disabled={params.currentDay ? true : false}
+//          >
+//           Day
+//        </NavLink>
+//       </div>
+//     </div>
+//   );
+// };
+
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import css from 'components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.module.css';
+// import { parseDate } from 'helpers/parseDate';
+// import { useParams } from 'react-router-dom';
+
+// export const PeriodTypeSelect = ({
+//   setSelectedDay,
+//   selectedDay,
+//   currentDate,
+//   setCurrentDate,
+// }) => {
 //   const params = useParams();
 //   const parsedDate = parseDate(currentDate);
 //   return (
@@ -25,7 +111,7 @@
 //             setCurrentDate(selectedDay);
 //           }}
 //         >
-//           {i18n.language === 'en' ? 'Month' : 'Місяць'}
+//           Month
 //         </NavLink>
 //         <NavLink
 //           className={`${css.types_tabs} ${
@@ -37,59 +123,74 @@
 //           }}
 //           disabled={params.currentDay ? true : false}
 //         >
-//           {i18n.language === 'en' ? 'Day' : 'День'}
+//           Day
+//         </NavLink>
+//         <NavLink
+//           className={`${css.types_tabs} ${
+//             window.location.pathname.includes('/day/') ? 'active' : ''
+//           }`}
+//           to={`day/${parsedDate}`}
+//           onClick={() => {
+//             if (selectedDay !== currentDate) {
+//               setSelectedDay(currentDate);
+//             }
+//           }}
+//           disabled={params.currentDay ? true : false}
+//         >
+//           Day
 //         </NavLink>
 //       </div>
 //     </div>
 //   );
 // };
 
-import React from 'react';
-import { NavLink, useLocation, useParams } from 'react-router-dom';
-import css from 'components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.module.css';
-import { parseDate } from 'helpers/parseDate';
+// import React from 'react';
+// import { NavLink, useLocation, useParams } from 'react-router-dom';
+// import css from 'components/Calendar/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.module.css';
+// import { parseDate } from 'helpers/parseDate';
 
-export const PeriodTypeSelect = ({
-  setSelectedDay,
-  selectedDay,
-  currentDate,
-  setCurrentDate,
-}) => {
-  const params = useParams();
-  const location = useLocation();
-  const parsedDate = parseDate(currentDate);
+// export const PeriodTypeSelect = ({
+//   setSelectedDay,
+//   selectedDay,
+//   currentDate,
+//   setCurrentDate,
+// }) => {
+//   const params = useParams();
+//   const location = useLocation();
+//   const parsedDate = parseDate(currentDate);
 
-  const isDayActive = location.pathname.includes('/day/');
-  const isMonthActive = !isDayActive;
+//   const isDayActive = location.pathname.includes('/day/');
+//   const isMonthActive = !isDayActive;
 
-  const handleMonthClick = () => {
-    setCurrentDate(selectedDay);
-  };
+//   const handleMonthClick = () => {
+//     setCurrentDate(selectedDay);
+//   };
 
-  const handleDayClick = () => {
-    setSelectedDay(currentDate);
-  };
+//   const handleDayClick = () => {
+//     setSelectedDay(currentDate);
+//   };
 
-  return (
-    <div className={css.tabs_flex}>
-      <div className={css.types_tabs_container}>
-        <NavLink
-          className={`${css.types_tabs} ${isMonthActive ? css.active : ''}`}
-          to="/calendar"
-          end
-          onClick={handleMonthClick}
-        >
-          Month
-        </NavLink>
-        <NavLink
-          className={`${css.types_tabs} ${isDayActive ? css.active : ''}`}
-          to={`day/${parsedDate}`}
-          onClick={handleDayClick}
-          disabled={!!params.currentDay}
-        >
-          Day
-        </NavLink>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className={css.tabs_flex}>
+//       <div className={css.types_tabs_container}>
+//         <NavLink
+//           className={`${css.types_tabs} ${isMonthActive ? css.active : ''}`}
+//           to="/calendar"
+//           end
+//           onClick={handleMonthClick}
+//         >
+//           Month
+//         </NavLink>
+//         <NavLink
+//           className={`${css.types_tabs} ${isDayActive ? css.active : ''}`}
+//           to={`day/${parsedDate}`}
+//           onClick={handleDayClick}
+//           disabled={!!params.currentDay}
+//         >
+//           Day
+//         </NavLink>
+//       </div>
+//     </div>
+//   );
+// };
+
--- src/pages/Calendar/CalendarPage.jsx
+++ src/pages/Calendar/CalendarPage.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router';
-// import React, { Suspense } from 'react';
 import { useParams } from 'react-router-dom';
 import { useState, Suspense, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import { CalendarToolbar } from 'components/Calendar/CalendarToolbar';
 import { ChoosedMonth } from 'pages/Calendar/ChoosedMonth/ChoosedMonth';
+import { ChoosedDay } from 'components/Calendar/ChoosedDay/ChoosedDay';
 import { setActivedDate } from 'redux/tasks/taskReducer';
 
 export const CalendarPage = ({ selectedDay, setSelectedDay }) => {
@@ -13,8 +13,8 @@ export const CalendarPage = ({ selectedDay, setSelectedDay }) => {
   const params = useParams();
   const [currentDate, setCurrentDate] = useState(new Date());
   useEffect(() => {
-    dispatch(setActivedDate(new Date().toISOString().slice(0, 10)));
-  }, [dispatch]);
+    dispatch(setActivedDate(currentDate.toISOString().slice(0, 10)));
+  }, [currentDate, dispatch]);
   return (
     <div>
       <Suspense fallback={null}>
@@ -24,13 +24,13 @@ export const CalendarPage = ({ selectedDay, setSelectedDay }) => {
           selectedDay={selectedDay}
           setSelectedDay={setSelectedDay}
         />
-        {!params?.currentDay && (
+        {!params?.currentDay ? (
           <ChoosedMonth
             setCurrentDate={setCurrentDate}
             currentDate={currentDate}
             setSelectedDay={setSelectedDay}
           />
-        )}
+        ) : (<ChoosedDay/>)}
         <Outlet />
       </Suspense>
     </div>
