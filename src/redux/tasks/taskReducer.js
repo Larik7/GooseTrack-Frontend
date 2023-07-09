7d05bdaa2973df45910a37b7a5aec1bdf51ff264
Dubei Anatolii <105717456+DubeiA@users.noreply.github.com>
2023-07-09 13:37:05 +0300

    correct reducer

--- src/redux/tasks/taskReducer.js
+++ src/redux/tasks/taskReducer.js
@@ -5,7 +5,7 @@ import { fetchTasks, addTask, updateTask, deleteTask } from './taskOperation';
 const taskReducer = createSlice({
   name: 'task',
   initialState: {
-    activeDate: '',
+    activeDate: new Date().toISOString().slice(0, 10),
     tasks: {
       allTasks: [],
       isLoading: false,
@@ -29,7 +29,6 @@ const taskReducer = createSlice({
   },
   reducers: {
     setActivedDate(state, { payload }) {
-      console.log(payload);
       state.activeDate = payload;
     },
     updateColumns: (state, action) => {
