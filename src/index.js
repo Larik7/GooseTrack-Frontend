3be995949aef3a32f4e74b4112d825291524363c
AS <samaloanastasia@gmail.com>
2023-07-10 18:48:31 +0300

    12,15

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store} >
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename="/GooseTrack-Frontend">
-          <TourProvider steps={steps}>
+          <TourProvider steps={steps} startAt={0}>
             <App />
           </TourProvider>
         </BrowserRouter>
